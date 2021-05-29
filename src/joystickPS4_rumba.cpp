3e6bb94886bab904113334961d591eba165087de
tamago117 <taikibannsei2020@gmail.com>
2021-05-29 01:54:30 -0700

    first commit

---
+++ src/joystickPS4_rumba.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+
+geometry_msgs::Twist cmd_vel;
+void joy_callback(const sensor_msgs::Joy& joy_msg)
+{
+  cmd_vel.linear.y = joy_msg.axes[4];
+  cmd_vel.angular.z = joy_msg.axes[0];
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "basic_twist_publisher");
+  ros::NodeHandle nh;
+  ros::Publisher cmd_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  ros::Subscriber joy_sub = nh.subscribe("joy", 10, joy_callback);
+
+  ros::Rate loop_rate(10);
+  while (ros::ok())
+  {
+    cmd_pub.publish(cmd_vel);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
